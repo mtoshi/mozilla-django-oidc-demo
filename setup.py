e9c4cb4d4be7cf4c1f85c15a8bcdd4a36a27464e
mtoshi <mtoshi@outlook.com>
2020-05-18 21:17:36 +0900

    Add base files

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""setup.py"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+requires = []
+with open('requirements.txt', 'w') as _file:
+    _file.write('\n'.join(requires))
+
+setup(
+    name='mozilla-django-oidc-demo',
+    version='0.0.0',
+    url='https://github.com/mtoshi/mozilla-django-oidc-demo',
+    author='mtoshi',
+    author_email='mtoshi@outlook.com',
+    description='Experimental app.',
+    packages=find_packages(),
+    install_requires=requires,
+)

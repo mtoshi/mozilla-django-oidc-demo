f9fa376c5df4abf2696990297c75b2e7f518951b
mtoshi <mtoshi@outlook.com>
2020-05-19 22:10:48 +0900

    Change home view and template

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.views.generic import View
 from django.shortcuts import render_to_response
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.sessions.models import Session
 
 
 class HomeView(LoginRequiredMixin, View):
@@ -13,7 +14,11 @@ class HomeView(LoginRequiredMixin, View):
 
     def get(self, request):
         """Get."""
+        sessionid = request.COOKIES['sessionid']
+        session = Session.objects.get(pk=sessionid)
+        session_data = session.get_decoded()
         return render_to_response('home/index.html', {
             'request': request,
             'settings': settings,
+            'session_data': session_data,
         }, status=200)

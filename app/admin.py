b76d348b8e0d6e8cf5f7c0f83fb04e664e628834
mtoshi <mtoshi@outlook.com>
2020-05-19 18:44:13 +0900

    Add session into admin view

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+"""app.admin."""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.sessions.models import Session
+
+
+class SessionAdmin(admin.ModelAdmin):
+
+    """SessionAdmin"""
+
+    list_display = (
+        'session_key',
+        '_session_data',
+        'expire_date')
+
+    readonly_fields = (
+        'session_key',
+        '_session_data',
+        'expire_date')
+
+    def _session_data(self, obj):
+        """Decode"""
+        return obj.get_decoded()
+
+
+admin.site.register(Session, SessionAdmin)
